4eb855543f28310e32712f7bff4cf4283e5609b2
MARK_POMERANT <mark.pomerant@oracle.com>
2017-01-13 19:48:47 -0500

    Add logo to team page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+const getColors = require('get-image-colors')
 
 // config files
 var app = express();
